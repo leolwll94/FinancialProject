b8acc85ae148baba35c4aa21b8bc24bf440b8610
leolwll94 <seu@email.com>
2025-11-19 17:00:44 -0300

    Implementação Tela Cadastro Usuario(HTML, css, Controller)

--- src/main/java/com/financialProject/Controller/CadastroUsuarioController.java
+++ src/main/java/com/financialProject/Controller/CadastroUsuarioController.java
@@ -2,18 +2,17 @@ package com.financialProject.Controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = {"/home"})
-public class HomeController {
+@RequestMapping(path = {"/CadastroUsuario"})
+public class CadastroUsuarioController {
 
     @GetMapping("")
     public String pageHome(){
 
-        return "Home";
+        return "CadastroUsuario";
     }
 
 }
